16ca09c183c2bb78fcf877947a4bb7bed20f287e
Marcel Assis <marcel@marcelassis.com>
2019-11-05 15:28:36 -0300

    add new portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do |blog|
   Blog.create!(
-    title: "My blog #{blog}" ,
+    title: My blog #{blog}" ,
     body: "This file is the source Rails uses to define your schema when running `rails db:schema:load`. When creating a new database, `rails db:schema:load` tends to be faster and is potentially less error prone than running all of your migrations from scratch. Old migrations may fail to apply correctly if those migrations use external dependencies or application code."
     )
 end
@@ -19,7 +19,7 @@ puts "5 skills posts created"
 
 9.times do |portfolio_item|
   Portfolio.create!(
-    title: "A title ${portfolio_item}" ,
+    title: "A title #{portfolio_item}",
     subtitle: "A little subtitle",
     body: "This file is the source Rails uses to define your schema when running `rails db:schema:load`. When creating a new database, `rails db:schema:load` tends to be faster and is potentially less error prone than running all of your migrations from scratch. Old migrations may fail to apply correctly if those migrations use external dependencies or application code." ,
     main_img:"https://via.placeholder.com/600x400" ,
